864716f317391972c8bd6f99cebfa63c7c872235
Agostina Gasparrino <111435290+agostinagasparrino@users.noreply.github.com>
2023-10-17 21:49:23 -0300

    Update ProfileControllerTest.java
    getProfileView, success y con exception

--- src/test/java/com/tallerwebi/presentacion/ProfileControllerTest.java
+++ src/test/java/com/tallerwebi/presentacion/ProfileControllerTest.java
@@ -3,17 +3,23 @@ package com.tallerwebi.presentacion;
 import com.tallerwebi.dominio.ProfileService;
 import com.tallerwebi.dominio.excepcion.UserNotFoundException;
 import com.tallerwebi.model.Notification;
+import com.tallerwebi.model.Parking;
+import com.tallerwebi.model.Vehicle;
+import com.tallerwebi.presentacion.dto.ProfileResponseDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
@@ -24,6 +30,8 @@ public class ProfileControllerTest {
     private ProfileService mockProfileService;
     @Mock
     private ProfileController profileController;
+    @Mock
+    private ProfileService profileService;
     @Mock
     private HttpSession mockHttpSession;
 
@@ -50,4 +58,34 @@ public class ProfileControllerTest {
         ModelAndView response = profileController.getNotifications();
         assertEquals("redirect:/error", response.getViewName());
     }
+
+    @Test
+    void testGetProfileViewSuccess(){
+        when(mockHttpSession.getAttribute("id")).thenReturn(123L);
+
+        Set<Vehicle> simulatedVehicles = new HashSet<>();
+        List<Parking> simulatedParkings = new ArrayList<>();
+
+        ProfileResponseDTO simulatedResponse = new ProfileResponseDTO(simulatedVehicles, simulatedParkings);
+
+        when(mockProfileService.getVehiclesAndParkingsByMobileUser(123L)).thenReturn(simulatedResponse);
+
+        ModelAndView modelAndView = profileController.getProfileView();
+
+        assertEquals("profile", modelAndView.getViewName());
+
+        ModelMap model = modelAndView.getModelMap();
+        assertTrue(model.containsAttribute("vehicles"));
+        assertTrue(model.containsAttribute("parkings"));
+    }
+
+    @Test
+    void testCannotGetProfileView(){
+        when(mockProfileService.getVehiclesAndParkingsByMobileUser(anyLong())).thenThrow(UserNotFoundException.class);
+        when(mockHttpSession.getAttribute("id")).thenReturn(456L);
+        ModelAndView modelAndView = profileController.getProfileView();
+        assertEquals("redirect:/error", modelAndView.getViewName());
+    }
+
 }
+
